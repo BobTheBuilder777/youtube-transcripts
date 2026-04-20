dd877c8b8a0f08e8a6419af14260d9ab2fc8a1e3
noel lau <noelatwork99@outlook.com>
2026-04-20 19:58:52 +0800

    Initial commit of transcript generator with readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# YouTube Transcript Generator
+## Function
+This python program takes as input a youtube video URL and generates a transcript, word count, and estimated reading time.
+
+## Installation
+1. Install the latest version of python
+2. Create a virtual environment `python -m venv venv`
+3. Activate the venv - For Mac/Linux: `source venv/bin/activate`, for windows: `venv\Scripts\activate`
+4. Run `pip install -r requirements.txt`
+
+## Usage
+1. Run `python3 transcript.py`
+2. Input YouTube video URL
---
+++ transcript.py
@@ -0,0 +1,58 @@
+from youtube_transcript_api import YouTubeTranscriptApi, NoTranscriptFound, VideoUnavailable
+from datetime import date
+import os
+
+# defining a function that takes video id and returns cleaned transcript
+def fetch_transcript(api, video_id):
+    transcript = api.fetch(video_id) #use the fetch method from the api class to get the transcript based on video id
+    
+    lines = []
+    for snippet in transcript:
+        lines.append(snippet.text.replace("\n", " "))
+            
+    clean_transcript = " ".join(lines)
+
+    return clean_transcript
+
+today = date.today()
+
+url = input("Input video URL: ")
+
+if "v=" not in url:
+    print("Invalid YouTube URL")
+    exit()
+
+parts = url.split("=", 1)
+
+video_id = parts[1]
+
+os.makedirs("transcripts", exist_ok=True) # make a directory called 'transcripts', only if it does not already exist
+filename = os.path.join("transcripts", f"{video_id}_{today}.txt") # Build the file path
+
+api = YouTubeTranscriptApi() #instantiate an instance of the YoutubeTranscriptApi class
+
+try:
+    clean_transcript = fetch_transcript(video_id)
+    word_count = len(clean_transcript.split())
+
+    adult_reading_time = 200 #words per minute
+    reading_time = round(word_count/adult_reading_time)
+
+    header = f"Video URL: {url}\nWord count: {word_count}\nEstimated Reading Time: {reading_time} minutes\n"
+    finished_transcript = header + clean_transcript
+
+    if not os.path.exists(filename):
+        with open(filename, "w") as f:
+            f.write(finished_transcript)
+
+        print(f"Transcript fetched!\nVideo URL:{url}\nWord count:{word_count}\nReading Time:{reading_time}\nSaved to {filename}")
+
+    else:
+        print(f"File already exist at {filename}")
+
+
+except NoTranscriptFound:
+    print(f"No captions found for this video — Whisper fallback coming soon!")
+except VideoUnavailable:
+    print(f"This video is unavailable or does not exist")
+
